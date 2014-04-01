a99ec9cf890af8dcfff0e75349c50bf12f35ae1f
Erwan Loisant <eloisant@gmail.com>
2014-04-01 13:54:25 +0200

    Cache and spinner

--- demo/src/main/java/io/prismic/android/app/MainActivity.java
+++ demo/src/main/java/io/prismic/android/app/MainActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.animation.AnimationUtils;
 import android.webkit.WebView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -20,15 +21,16 @@ import java.util.Map;
 
 public class MainActivity extends ActionBarActivity {
 
+  private DemoApplication app;
+
   private Map<String, String> bookmarks;
   private String[] bookmarksArray;
+
   private DrawerLayout mDrawerLayout;
   private ActionBarDrawerToggle mDrawerToggle;
   private ListView mDrawerList;
-
-  // It's better to have only one Prismic instance in your app - if you have more than one Activity,
-  // it should go to a custom Application class
-  public Prismic prismic = new Prismic("https://lesbonneschoses.prismic.io/api");
+  private View mLoading;
+  private WebView mWebView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -39,18 +41,21 @@ public class MainActivity extends ActionBarActivity {
     mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
     mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.drawable.ic_drawer, R.string.drawer_open, R.string.drawer_close);
     mDrawerLayout.setDrawerListener(mDrawerToggle);
+    mLoading = findViewById(R.id.loading);
+    mWebView = (WebView)findViewById(R.id.webView);
+
     getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     getSupportActionBar().setHomeButtonEnabled(true);
 
-    prismic.init();
-    prismic.registerListener(apiListener);
+    app = (DemoApplication)getApplication();
+    app.prismic.registerListener(apiListener);
   }
 
   private Prismic.Listener<Api> apiListener = new Prismic.Listener<Api>() {
     @Override
     public void onSuccess(Api result) {
-      Log.v("prismic", "API is ready from activity: " + prismic);
-      bookmarks = prismic.getBookmarks();
+      Log.v("prismic", "API is ready");
+      bookmarks = app.prismic.getBookmarks();
       bookmarksArray = bookmarks.keySet().toArray(new String[0]);
       loadBookmark("about");
       mDrawerList.setAdapter(new ArrayAdapter<String>(MainActivity.this, R.layout.drawer_list_item, bookmarksArray));
@@ -58,14 +63,31 @@ public class MainActivity extends ActionBarActivity {
   };
 
   private void loadBookmark(String key) {
-     prismic.getBookmark(key, new Prismic.Listener<Document>() {
+    setLoading(true);
+    app.prismic.getBookmark(key, new Prismic.Listener<Document>() {
       @Override
       public void onSuccess(Document result) {
         String html = result.asHtml(new BlankLinkResolver());
-        WebView myWebView = (WebView)MainActivity.this.findViewById(R.id.webView);
-        myWebView.loadDataWithBaseURL(null, html, null, "UTF-8", null);
+        mWebView.loadDataWithBaseURL(null, html, null, "UTF-8", null);
+        setLoading(false);
+      }
+      @Override
+      public void onError(Api.Error error) {
+        setLoading(false);
       }
     });
+  }
+
+  private void setLoading(Boolean loading) {
+    if (loading) {
+      mLoading.setVisibility(View.VISIBLE);
+      mWebView.setVisibility(View.GONE);
+    } else {
+      mLoading.startAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_out));
+      mWebView.startAnimation(AnimationUtils.loadAnimation(this, android.R.anim.fade_in));
+      mLoading.setVisibility(View.GONE);
+      mWebView.setVisibility(View.VISIBLE);
+    }
   }
 
   private class DrawerItemClickListener implements ListView.OnItemClickListener {
